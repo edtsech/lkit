19ad434a8b815bfd02e274f31fe9e21c19eb40a3
Edward Tsech <edtsech@gmail.com>
2013-03-01 09:48:33 +0100

    Initial commit

---
+++ project.clj
@@ -0,0 +1,22 @@
+(defproject lkit "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [lib-noir "0.4.8"]
+                 [compojure "1.1.5"]
+                 [hiccup "1.0.2"]
+                 [ring-server "0.2.7"]                 
+                 [com.taoensso/timbre "1.5.1"]
+                 [com.taoensso/tower "1.2.0"]
+                 [markdown-clj "0.9.19"]]  
+  :plugins [[lein-ring "0.8.2"]]
+  :ring {:handler lkit.handler/war-handler
+         :init    lkit.handler/init
+         :destroy lkit.handler/destroy}  
+  :profiles
+  {:production {:ring {:open-browser? false 
+                       :stacktraces?  false 
+                       :auto-reload?  false}}
+   :dev {:dependencies [[ring-mock "0.1.3"]
+                        [ring/ring-devel "1.1.8"]]}}
+  :min-lein-version "2.0.0")
